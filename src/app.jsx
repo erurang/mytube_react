90d8510f1b0259ed9b23475e628e2207c983d11c
erurang <erurang@naver.com>
2021-01-09 11:29:58 +0900

    view기능에 view만 둘수있도록 service를 따로 만들어서 네트워크 통신 로직을 처리함.(유닛테스트를 위해 나누는게 좋음)

--- src/app.jsx
+++ src/app.jsx
@@ -3,47 +3,34 @@ import Header from "./components/header/header";
 import VideoList from "./components/video_list/video_list";
 import styles from "./app.module.css"
 
-function App() {
+function App({ youtube }) {
 
   // state ={} 와 같음. [변수명,변화를 적용할 변수명]
   const [videos, setVideos] = useState([]);
 
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    youtube//
+      .search(query)//
+      .then(videos => setVideos(videos))
 
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=AIzaSyCnPALRYfXJ74c2BugvWhTGefTWhYQEwNQ`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
   }
 
   // didmount upadate의 2가지 기능을 가지고있음 , 2번째 인자는 어떤것이 업데이트될때 2가지 기능을 업데이트할건지 정함
   useEffect(() => {
-
-    // fetch의 기본설정
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=KR&key=AIzaSyCnPALRYfXJ74c2BugvWhTGefTWhYQEwNQ", requestOptions)
-      // response.text 는 text형태로오는데
-      // json으로 변경하여 값에 접근하기 더 쉽게변경함
-      .then(response => response.json())
-      // setVideos로 상태설정을함
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    console.log("useEffect");
+    youtube//
+      .mostPopular() //
+      .then(videos => setVideos(videos))
   }, [])
 
 
-  return <div className={styles.app}>
-    <Header onSearch={search} />
-    <VideoList videos={videos} />
-  </div>
+
+  return (
+    <div className={styles.app}>
+      <Header onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  )
 }
 
 export default App
---
+++ src/service/youtube.js
@@ -0,0 +1,33 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+
+  mostPopular() {
+    return (
+      fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=KR&key=${this.key}`,
+        this.getRequestOptions
+      )
+        // response.text 는 text형태로오는데
+        // json으로 변경하여 값에 접근하기 더 쉽게변경함
+        .then((response) => response.json())
+        .then((result) => result.items)
+    );
+  }
+
+  async search(query) {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result = await response.json();
+    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+  }
+}
+
+export default Youtube;
